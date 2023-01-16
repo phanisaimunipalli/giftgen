a908d569c06f4a3e3747c93131d8f55342b711b8
phanisaimunipalli <phanisaimunipalli@gmail.com>
2023-01-15 20:48:30 -0800

    PSR - Added circularprogress for loading

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,12 @@ import React from "react";
 import { useState } from "react";
 import styles from "./index.module.css";
 
+import { CircularProgress } from "@mui/material";
 import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
+import { useEffect } from "react";
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
@@ -24,8 +26,15 @@ export default function Home() {
   const [priceMax, setPriceMax] = useState(100);
   const [hobbies, setHobbies] = useState("");
   const [loading, setLoading] = useState(false);
+  const [showResult, setShowResult] = useState(false);
 
   const [result, setResult] = useState("");
+
+  useEffect(() => {
+    if (loading) {
+      setShowResult(true);
+    }
+  }, [loading]);
 
   async function onSubmit(event) {
     event.preventDefault();
@@ -73,87 +82,92 @@ export default function Home() {
           >
             <Grid item md={4} textAlign="center">
               <Paper elevation={24}>
-                <form
-                  style={{ margin: "auto", minHeight: "20vh" }}
-                  onSubmit={onSubmit}
-                  textAlign="center"
-                >
-                  <label>Occasion or Event</label>
-                  <input
-                    type="text"
-                    name="ocassion"
-                    placeholder="Enter the occassion or event..."
-                    value={ocassion}
-                    onChange={(e) => setOcassion(e.target.value)}
-                  />
-                  <label>For who is the gift?</label>
-                  <select
-                    name="gender"
-                    value={gender}
-                    onChange={(e) => setGender(e.target.value)}
-                  >
-                    <option value="man">Man</option>
-                    <option value="woman">Woman</option>
-                    <option value="woman">Couple</option>
-                  </select>
-
-                  <label>Age</label>
-                  <input
-                    type="number"
-                    min={1}
-                    max={99}
-                    name="age"
-                    placeholder="Enter the age"
-                    value={age}
-                    onChange={(e) => setAge(Number.parseInt(e.target.value))}
-                  />
-
-                  <label>Price from</label>
-                  <input
-                    type="number"
-                    min={1}
-                    name="priceMin"
-                    placeholder="Enter the minimum price"
-                    value={priceMin}
-                    onChange={(e) =>
-                      setPriceMin(Number.parseInt(e.target.value))
-                    }
-                  />
-
-                  <label>Price to</label>
-                  <input
-                    type="number"
-                    min={1}
-                    name="priceMax"
-                    placeholder="Enter the maximum price"
-                    value={priceMax}
-                    onChange={(e) =>
-                      setPriceMax(Number.parseInt(e.target.value))
-                    }
-                  />
-
-                  <label>Hobbies</label>
-                  <input
-                    type="text"
-                    name="hobbies"
-                    placeholder="Enter the hobbies"
-                    value={hobbies}
-                    onChange={(e) => setHobbies(e.target.value)}
-                  />
-                  <input type="submit" value="Generate gift ideas" />
-                </form>
                 <Grid item xs={8} md={6}>
                   <Item>
-                    {loading && (
+                    {loading ? (
                       <div>
-                        <h3>Looking for the best gift ideas... 🎁 💡</h3>
-                        <img src={require("/images/loading.png")} />
+                        <p>Looking for the best gift ideas...</p>
+                        <CircularProgress />
                       </div>
+                    ) : (
+                      <div
+                        className={styles.result}
+                        dangerouslySetInnerHTML={{ __html: result }}
+                      />
                     )}
-                    <div
-                      className={styles.result}
-                      dangerouslySetInnerHTML={{ __html: result }}
-                    />
+                    {!loading && (
+                      <form
+                        style={{ margin: "auto", minHeight: "20vh" }}
+                        onSubmit={onSubmit}
+                        textAlign="center"
+                      >
+                        <label>Occasion or Event</label>
+                        <input
+                          type="text"
+                          name="ocassion"
+                          placeholder="Enter the occassion or event..."
+                          value={ocassion}
+                          onChange={(e) => setOcassion(e.target.value)}
+                        />
+                        <label>For who is the gift?</label>
+                        <select
+                          name="gender"
+                          value={gender}
+                          onChange={(e) => setGender(e.target.value)}
+                        >
+                          <option value="man">Man</option>
+                          <option value="woman">Woman</option>
+                          <option value="woman">Couple</option>
+                        </select>
+
+                        <label>Age</label>
+                        <input
+                          type="number"
+                          min={1}
+                          max={99}
+                          name="age"
+                          placeholder="Enter the age"
+                          value={age}
+                          onChange={(e) =>
+                            setAge(Number.parseInt(e.target.value))
+                          }
+                        />
+
+                        <label>Price from</label>
+                        <input
+                          type="number"
+                          min={1}
+                          name="priceMin"
+                          placeholder="Enter the minimum price"
+                          value={priceMin}
+                          onChange={(e) =>
+                            setPriceMin(Number.parseInt(e.target.value))
+                          }
+                        />
+
+                        <label>Price to</label>
+                        <input
+                          type="number"
+                          min={1}
+                          name="priceMax"
+                          placeholder="Enter the maximum price"
+                          value={priceMax}
+                          onChange={(e) =>
+                            setPriceMax(Number.parseInt(e.target.value))
+                          }
+                        />
+
+                        <label>Hobbies</label>
+                        <input
+                          type="text"
+                          name="hobbies"
+                          placeholder="Enter the hobbies"
+                          value={hobbies}
+                          onChange={(e) => setHobbies(e.target.value)}
+                        />
+                        <input type="submit" value="Generate gift ideas" />
+                      </form>
+                    )}
                   </Item>
                 </Grid>
               </Paper>
